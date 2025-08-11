8a45ceb91f9ee28da6cb5204022e3e586a9603dc
listmen001 <za.ignatev@gmail.com>
2025-08-11 12:35:54 +0400

    g7

---
+++ g7.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+numbers = input("Введите числа через пробел: ").split()
+numbers = [int(x) for x in numbers]
+sorted_numbers = bubble_sort(numbers)
+print(f"Отсортированный список: {sorted_numbers}")
